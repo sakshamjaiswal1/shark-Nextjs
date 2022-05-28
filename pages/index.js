6d044c2295b7389e4428a740d0e4ccfdfcbc75ed
Saksham Jiaswal <sjais973@gmail.com>
2022-05-29 03:49:05 +0530

    sort

--- pages/index.js
+++ pages/index.js
@@ -30,11 +30,38 @@ export default function Home({ data ,rank}) {
       setSel('rank')
     }
   }
+  const increment =()=>{
+    setCount(count+10)
+
+  }
+  const decrement =()=>{
+    if(count>10){
+      setCount(count-10)
+  
+    }
+
+  }
+  const reset =()=>{
+    setCount(10)
+
+  }
   return (
     <div className={styles.mainCon}>
       <div className={styles.tilesCon}>
         <div className={styles.tilesCon__topCon}>
-          <h3>Listings ({count})</h3>
+        
+          <h3>Listings ({count}) &nbsp; <Image
+          src='/info.png'
+          alt="Picture of the author"
+          width={16}
+          height={16}
+          padding={5}
+          className={styles.indoImg}
+        /> </h3>
+         
+           
+            
+           
 
           <div className={styles.custom_select}>
             <select value={sel}  onChange={(e)=>handleSort(e)} className={styles.select}>
@@ -46,7 +73,7 @@ export default function Home({ data ,rank}) {
         </div>
 
         <div className={styles.tilesCon__midCon}>
-          {sortArray.map((elem, index) => {
+          {data.prices.slice(0,count).map((elem, index) => {
             
             return(
             <>
@@ -60,6 +87,12 @@ export default function Home({ data ,rank}) {
               />
             </>)
           })}
+        </div>
+        <div className={styles.btn_Container}>
+          <button onClick={reset} >R</button>
+          <button onClick={decrement} >-</button>
+          <button onClick={increment} >+</button>
+
         </div>
       </div>
     </div>
